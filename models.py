a61048daae20a8272f9751936095a3c6ef4a4d00
Joseph Mennuti <josephmennuti@Josephs-MacBook-Pro-2.local>
2022-03-18 19:37:58 -0400

    Fix __str__() in NearEarthObject class

--- models.py
+++ models.py
@@ -68,7 +68,10 @@ class NearEarthObject:
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"A NearEarthObject ..."
+        if self.hazardous:
+            return f"NEO {self.fullname} has a diameter of{self.diameter:.2f} km and is hazardous."
+        else:
+            return f"NEO {self.fullname} has a diameter of{self.diameter:.2f} km and is not hazardous."
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
